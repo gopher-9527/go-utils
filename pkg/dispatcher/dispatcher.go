df872dd098bef0739af9c202786469931a933299
dingyuanwu <dingyuan.wu@shopee.com>
2025-05-29 17:06:34 +0800

    add dispatcher

--- pkg/dispatcher/dispatcher.go
+++ pkg/dispatcher/dispatcher.go
@@ -1,5 +1,63 @@
 package dispatcher
 
-type Dispatcher interface {
-	Dispatch() error
+import "sync"
+
+type Job func() any
+
+type Dispatcher interface{}
+
+type dispatcher struct {
+	jobQueue chan Job
+	workers  int
+	result   chan any
+	done     chan struct{}
+	closed   bool
+	lock     sync.Mutex
+}
+
+func (d *dispatcher) NewDispatch(queueLength, workers int) Dispatcher {
+	return &dispatcher{
+		jobQueue: make(chan Job, queueLength),
+		workers:  workers,
+		done:     make(chan struct{}),
+		result:   make(chan any),
+	}
+}
+
+func (d *dispatcher) Run() (chan any, chan struct{}) {
+	d.closed = false
+	for i := 0; i < d.workers; i++ {
+		go func() {
+			for {
+				select {
+				case job := <-d.jobQueue:
+					r := job()
+					d.result <- r
+				case <-d.done:
+					return
+				}
+			}
+		}()
+	}
+	return d.result, d.done
+}
+
+func (d *dispatcher) AddJob(job Job) {
+	if d.closed {
+		return
+	}
+	d.jobQueue <- job
+}
+
+func (d *dispatcher) Close() {
+	d.lock.Lock()
+	defer d.lock.Unlock()
+
+	if d.closed {
+		return
+	}
+	d.closed = true
+	close(d.done)
+	close(d.jobQueue)
+	close(d.result)
 }
---
+++ pkg/dispatcher/readme.md
@@ -0,0 +1,6 @@
+# dispatcher
+
+1. 一次添加所有job
+2. 后续可以继续追加job
+3. 定量的worker处理job，控制goroutine的数量
+4. 
